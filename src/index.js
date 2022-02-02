56e5d3b6387b1848a871de9aac7325024014349f
Eric Crudup <briefcasemanx@gmail.com>
2022-02-02 13:56:24 -0800

    added UI styling and logic
    Co-Authored-By: Matthew Eilar <meilar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import dayNamer from './js/dayNamer.js';
 
 
 
 $(document).ready(function() {
+  $("#start-button").click( function(){
+    const datePicked = $("#date-picker").val();
+    
+
+    const dateArr = datePicked.split("-");
+
+    const dayOfWeek = dayNamer(dateArr[0], dateArr[1], dateArr[2]);
+  
+    $("#input").addClass("hidden");
+    $("#output").removeClass("hidden");
+    $("#output-date").text(datePicked);
+    $("#output-day").text(dayOfWeek);
+  });
+
+  $("#reset-button").click( function(){
+    location.reload(true);
+  });
 });
