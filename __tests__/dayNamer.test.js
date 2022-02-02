6de56042b366e7db5894d2eb13fce53f52551225
Eric Crudup <briefcasemanx@gmail.com>
2022-02-02 11:55:33 -0800

    dayNamer returns day of the week
    Co-Authored-By: Matthew Eilar <meilar@gmail.com>

--- __tests__/dayNamer.test.js
+++ __tests__/dayNamer.test.js
@@ -2,8 +2,14 @@ import dayNamer from '../src/js/dayNamer.js';
 
 describe('dayNamer', () => {
 
-  test('should return a day of the week index number', () =>{
-    let newDate = dayNamer(2022, 1, 2);
-    expect(newDate.getDate()).toEqual(2);
+  test('should return a day of the week', () =>{
+    expect(dayNamer(2022, 3, 2)).toEqual(expect.any(String));
   });
+
+  test('should return weekday name for a given date', () =>{
+    let output = dayNamer(2022, 3, 2);
+    expect(output).toEqual("Saturday");
+  });
+
+  
 });
--- src/js/dayNamer.js
+++ src/js/dayNamer.js
@@ -1,5 +1,7 @@
-export default function dayNamer() {
-  let date = new Date();
-  return date;
+export default function dayNamer(year, month, day) {
+  let date = new Date(year, month, day);
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  return weekdays[date.getDay()];
 }
 
